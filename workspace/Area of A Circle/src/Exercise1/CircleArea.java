84e85302bea393e378aae3f017c67ba098f184a3
Bettye <bwilliams523@hotmail.com>
2015-09-03 14:15:50 -0700

    AOAC

---
+++ workspace/Area of A Circle/src/Exercise1/CircleArea.java
@@ -0,0 +1,18 @@
+package Exercise1;
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		 int radius = 3;
+	      double area = Math.PI * (radius * radius);
+	      System.out.println("The area of circle is: " + area);
+	      double circumference= Math.PI * 2*radius;
+	      System.out.println( "The circumference of the circle is:"+circumference) ;
+	   }
+		   }
+	
+
+
