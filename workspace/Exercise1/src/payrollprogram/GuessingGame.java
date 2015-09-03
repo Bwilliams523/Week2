737f3425f37bb6f08779f9903c0ca932fcca3a2e
Bettye <bwilliams523@hotmail.com>
2015-09-03 15:05:28 -0700

    Updated Guessing Game

--- workspace/Exercise1/src/payrollprogram/GuessingGame.java
+++ workspace/Exercise1/src/payrollprogram/GuessingGame.java
@@ -7,6 +7,22 @@ public class GuessingGame {
 		System.out.println ("I am thinking of a number from 1 to 10." 
 				+ "You must guess what it is in three tries. Enter a guess:");
 		
+		 Scanner scan = new Scanner(System.in);
+	        boolean wrong = 4;
+	        boolean right = 8;
+	        System.out.println("You won the game: ");
+	        
+	     
+	        System.out.println ("I am thinking of a number from 1 to 10." 
+					+ "You must guess what it is in three tries. Enter a guess:");
+	        
+	        Scanner scan = new Scanner(System.in);
+	        boolean wrong = 1;
+	        boolean wrong = 5
+	        boolean wrong = 9;
+	        System.out.println("The correct number was 7 + You lost the game.: ");
+			
+		
 		
 		
 
